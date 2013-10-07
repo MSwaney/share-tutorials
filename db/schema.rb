173b6e837adc2d3a525002190accb17eb7dfb9b9
epicodus <student@epicodus.com>
2013-10-07 12:13:06 -0700

    Post and view tutorials

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131007171403) do
+ActiveRecord::Schema.define(version: 20131007183405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tutorials", force: true do |t|
+    t.string   "description"
+    t.string   "link"
+    t.integer  "user_id"
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
