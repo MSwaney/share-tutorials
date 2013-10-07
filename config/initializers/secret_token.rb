5fac0cb38734b398b0888c8c8658c9baf86292ff
epicodus <student@epicodus.com>
2013-10-07 10:23:25 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tic::Application.config.secret_key_base = 'ad18ea7f6e536d9ce97630d3443a17d021e3d15762cdf050d56565d480b5214f2fa737a2c6dc29879e24220c9a1cdde5793105bb94450b96f777098c06e8f451'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tic::Application.config.session_store :cookie_store, key: '_tic_session'
