eb969692179b31160019bd16d50f96b071e11a78
epicodus <student@epicodus.com>
2013-10-09 17:28:00 -0700

    Nicer buttons, fix errors

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,9 @@
 module CommentsHelper
+  def comment_link_helper(tutorial)
+    if tutorial.comments.count > 0
+      link_to(pluralize(tutorial.comments.count, "comment"), tutorial_path(tutorial.id))
+    else
+      link_to "Leave the first comment!", tutorial_path(tutorial.id)
+    end
+  end
 end
